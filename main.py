ea27ff520a5990a41de9f95ac3e390cf0334814c
Sam Reece <samreece_99@hotmail.com>
2020-03-06 17:10:03 +0000

    Commiting first draft of the readules, main, geodist (given by lecturers), as well as the data we are provided with.

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+
+if __name__ == "__main__":
+    # James' UI's returns
+    # Goes to Sam's file reader
+    # Gives input to Roxy's data sorter and filterer
+    # Gives data to Samir's file creator
