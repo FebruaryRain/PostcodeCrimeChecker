b9fcb20ff3db7a62367bd62ca7233f5bc6c94b46
Sam Reece <samreece_99@hotmail.com>
2020-03-07 17:34:12 +0000

    added testing for the geodist.py file, 10 test cases comparing 5 points against two other points. For issue 7

--- geodist.py
+++ geodist.py
@@ -25,6 +25,46 @@ def distance( latlngA, latlngB):
     d = math.sqrt( x*x + y*y ) * R
     return d
 
+## Tests
+def distance_should_return_correct_distances_when_given_two_valid_points():
+    list_of_lat_and_longs_1 = [
+        (-2.44427954,50.71527036),
+        (-4.40200942,50.97137163)
+    ]
+    list_of_lat_and_longs_2 = [
+        (-4.544128,50.829232),
+        (-4.714629,50.462646),
+        (-4.467377,50.457415),
+        (-5.057535,50.421859),
+        (-3.988073,50.740558)
+    ]
+    list_of_lat_long_returns_1 = []
+    list_of_lat_long_expected_1 = [
+        233.83515970684675, 
+        254.00367188668648, 
+        226.77176534446895, 
+        292.39922075200764, 
+        171.6851984351975
+    ]
+
+    list_of_lat_long_returns_2 = []
+    list_of_lat_long_expected_2 = [
+        22.316295380280835, 
+        66.24261329441788, 
+        57.44009423308353, 
+        94.98070603992166, 
+        52.666246628324394
+    ] 
+    for coord in list_of_lat_and_longs_2:
+        list_of_lat_long_returns_1.append(distance(list_of_lat_and_longs_1[0], coord))
+    for coord in list_of_lat_and_longs_2:
+        list_of_lat_long_returns_2.append(distance(list_of_lat_and_longs_1[1], coord))
+    assert(list_of_lat_long_returns_1 == list_of_lat_long_expected_1)
+    assert(list_of_lat_long_returns_2 == list_of_lat_long_expected_2)
+    #print(list_of_lat_long_returns_1)
+    #print(list_of_lat_long_returns_2)
+    return
+
 if __name__ == "__main__":
     # Testing
     # TODO: Please verify the function by providing suitable test cases and
@@ -32,3 +72,6 @@ if __name__ == "__main__":
     # with, e.g. do not verify the correct distance between New York and London
     # For your testing allow a tolerance for the distance calculation as the
     # calculation is an approximation.
+
+    #print(distance( (-4.544128,50.829232), (-4.544117,50.827973)))
+    distance_should_return_correct_distances_when_given_two_valid_points()
