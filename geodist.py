10a2598b546b5c181979132604d40affa236de62
Sam D Reece <sam.reece@ibm.com>
2020-03-28 14:53:38 +0000

    It works. Numerous small tweaks, including adding error handling for if the user already has a file of the same name specified open in Excel.

--- geodist.py
+++ geodist.py
@@ -94,4 +94,5 @@ if __name__ == "__main__":
     # calculation is an approximation.
 
     distance_should_return_correct_distances_when_given_two_valid_points()
+    print(distance( (50.81863386, -4.54980488), (50.828441, -4.551129)))
     
